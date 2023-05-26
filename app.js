6ed2cb52d2384c56dbde6e60f2de4cf907fbaae4
wevdiaz <63312710+wevdiaz@users.noreply.github.com>
2023-05-25 21:26:51 -0300

    check deploy update

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require("express");
-const routes = require("./routes");
-const nunjucks = require("nunjucks");
-const app = express();
-const cors = require("cors");
+const express = require("express")
+const routes = require("./routes")
+const nunjucks = require("nunjucks")
+const app = express()
+const cors = require("cors")
 
-app.use(express.static("public"));
+app.use(express.static("public"))
+
+app.set("view engine", "html")
 
 nunjucks.configure("views", {
   express: app,
   autoescape: true,
-  noCache: true
-});
+  noCache: true,
+})
 
-app.use(cors());
-app.use(routes);
+app.use(cors())
+app.use(routes)
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("http://localhost:3000");
-  console.log("Server is working!");
-});
+  console.log("http://localhost:3000")
+  console.log("Server is working!")
+})
